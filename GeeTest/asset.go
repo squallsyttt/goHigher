42581b9d4afde182e785073c04e94481311bdf89
squallsyttt <390239178@qq.com>
2022-01-24 13:52:15 +0800

    0124 up

---
+++ GeeTest/asset.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	var w io.Writer
+	w = os.Stdout
+
+	w.Write([]byte("hello Go!"))
+	fmt.Printf("%T\n", w)
+
+	fw := w.(*os.File)
+	fmt.Printf("%T\n", fw)
+}

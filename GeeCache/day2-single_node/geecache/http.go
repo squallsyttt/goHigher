924a5a8acdeac2b46fa056a9818a10d76a9ffbe0
squallsyttt <390239178@qq.com>
2022-02-10 11:45:24 +0800

    2.10 push in tun mode

--- GeeCache/day2-single_node/geecache/http.go
+++ GeeCache/day2-single_node/geecache/http.go
@@ -1,1 +1,33 @@
 package geecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const defaultBasePath = "/_geecache/"
+
+type HTTPPool struct {
+	self     string
+	basePath string
+}
+
+func NewHTTPPool(self string) *HTTPPool {
+	return &HTTPPool{
+		self:     self,
+		basePath: defaultBasePath,
+	}
+}
+
+func (p *HTTPPool) Log(format string, v ...interface{}) {
+	log.Printf("[Server %s] %s", p.self, fmt.Sprintf(format, v...))
+}
+
+func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !strings.HasPrefix(r.URL.Path, p.basePath) {
+		panic("HTTPPool serving unexpected path: " + r.URL.Path)
+	}
+	p.Log("%s %s", r.Method, r.URL.Path)
+}
